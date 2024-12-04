e8ec8b0ad98b020bd93f486816bef674287f3041
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2024-12-04 09:30:51 -0500

    Many Changes (Background and outputs)

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ let userQuestion = prompt('What is your question for the Magic 8 Ball')
   //output based on random number generator
   if (randomNumber == 1){
     console.log('It is certain')
-eightBall = ''
-document.getElementById("response-image").src = ''
+eightBall = 'It is certain'
+document.getElementById("response-image").src = 'imgs/Goddess_Hylia_statue_M8B.webp'
   }
   else if (randomNumber == 2) {
     console.log('It is decidedly so')
@@ -24,8 +24,8 @@ document.getElementById("response-image").src = 'imgs/Decided-M8.png'
   }
   else if (randomNumber == 3) {
     console.log('Try again Later')
-eightBall = ''
-document.getElementById("response-image").src = ''
+eightBall = 'I AM ERROR (try again later...)'
+document.getElementById("response-image").src = 'imgs/I_am_Error-M8.png'
   }
   else if (randomNumber == 4) {
     console.log('Cannot predict now')
@@ -34,13 +34,13 @@ document.getElementById("response-image").src = ''
   }
   else if (randomNumber == 5) {
     console.log('Do not count on it')
-eightBall = ''
-document.getElementById("response-image").src = ''
+eightBall = 'Dont count on it'
+document.getElementById("response-image").src = 'imgs/Lonk.jpg'
   }
   else if (randomNumber == 6) {
     console.log('My sources say No')
 eightBall = 'No'
-document.getElementById("response-image").src = ''
+document.getElementById("response-image").src = 'imgs/no-legend-of-zelda-M8B.gif'
   }
   else if (randomNumber == 7) {
     console.log('Outlook not so good')
@@ -55,5 +55,5 @@ document.getElementById("response-image").src = 'imgs/Terrible-Fate-8B.png'
 
   
   console.log(eightBall)
-  document.getElementById('response-text').innerText = `${userName} has asked the Magic 8 Ball ${userQuestion} The wise and wondrous 8 ball has replied ${eightBall}`
+  document.getElementById('response-text').innerText = `${userName} has asked the Deku Tree, ${userQuestion} The wise and Great Deku Tree has replied ${eightBall}`
 }
